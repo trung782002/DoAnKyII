ef56b40f122a4afca09a65320798d684ffa71ca8
KimTrung <89015801+trung782002@users.noreply.github.com>
2022-04-22 16:06:16 +0700

    trung

--- WebService/src/main/java/bkap/services/AccountService.java
+++ WebService/src/main/java/bkap/services/AccountService.java
@@ -3,12 +3,12 @@ package bkap.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
